69750df3971beabc64770e15af24d8ae7cd32a73
cheininger <christoph_heininger@hotmail.com>
2023-09-05 23:50:08 +0200

    added basic functionality
    added two buttons to toggle between drawing and erasing mode

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 const canvas = document.querySelector(".canvas");
+const drawButton = document.querySelector("button.draw");
+const eraseButton = document.querySelector("button.erase");
+const squares = document.querySelectorAll(".square");
 
 for (let i = 1; i <= 16 * 16; i++) {
   const square = document.createElement("div");
   square.setAttribute("class", `square square-${i}`);
   canvas.appendChild(square);
 }
+
+drawButton.addEventListener("click", function () {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      squares[i].classList.add("hover");
+    });
+  }
+});
+
+eraseButton.addEventListener("click", function () {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      squares[i].classList.remove("hover");
+    });
+  }
+});
