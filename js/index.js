8340331fbb9ad17a102327c9585505a299a564b9
cheininger <christoph_heininger@hotmail.com>
2023-10-03 21:37:32 +0200

    resolved issues in index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 const canvas = document.querySelector(".canvas");
-const drawButton = document.querySelector("button.draw");
-const eraseButton = document.querySelector("button.erase");
-const squares = document.querySelectorAll(".square");
 
 for (let i = 1; i <= 16 * 16; i++) {
   const square = document.createElement("div");
