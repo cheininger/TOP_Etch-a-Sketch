fc8f995ed5029e0f7ec6f03e5b9158a122cc6428
cheininger <christoph_heininger@hotmail.com>
2023-10-03 21:49:18 +0200

    added draw and erase button

--- js/index.js
+++ js/index.js
@@ -8,8 +8,22 @@ for (let i = 1; i <= 16 * 16; i++) {
 
 const squares = document.querySelectorAll(".square");
 
-for (let i = 0; i < squares.length; i++) {
-  squares[i].addEventListener("mouseover", function () {
-    squares[i].classList.add("black");
-  });
-}
+const drawButton = document.querySelector(".button-draw");
+const eraseButton = document.querySelector(".button-erase");
+
+drawButton.addEventListener("click", () => {
+  console.log("clicked");
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      squares[i].classList.add("black");
+    });
+  }
+});
+
+eraseButton.addEventListener("click", () => {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", function () {
+      squares[i].classList.remove("black");
+    });
+  }
+});
