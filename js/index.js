b0db3728f0edf1623568dfed852798203b0b6986
cheininger <christoph_heininger@hotmail.com>
2023-10-03 22:41:43 +0200

    added dysfunctional resize button
    resize button correctly changes the grid, but draw and erase buttons don't work anymore!?

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const squares = document.querySelectorAll(".square");
 
 const drawButton = document.querySelector(".button-draw");
 const eraseButton = document.querySelector(".button-erase");
+const resizeButton = document.querySelector(".button-resize");
 
 drawButton.addEventListener("click", () => {
   console.log("clicked");
@@ -25,5 +26,41 @@ eraseButton.addEventListener("click", () => {
     squares[i].addEventListener("mouseover", function () {
       squares[i].classList.remove("black");
     });
+  }
+});
+
+resizeButton.addEventListener("click", () => {
+  const size = prompt(
+    "How big (X x X) should the grid be? Please stay below 100!"
+  );
+
+  if (size > 100) {
+    alert("You entered a number avoe 100. Please try again.");
+  } else {
+    console.log(size);
+
+    while (canvas.firstChild) {
+      canvas.removeChild(canvas.firstChild);
+    }
+
+    for (let i = 1; i <= size * size; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("class", `square square-${i}`);
+      canvas.appendChild(square);
+    }
+
+    const stylesheet = document.styleSheets[0];
+    let elementRules;
+
+    for (let i = 0; i < stylesheet.cssRules.length; i++) {
+      if (stylesheet.cssRules[i].selectorText === ".square") {
+        elementRules = stylesheet.cssRules[i];
+      }
+    }
+
+    elementRules.style.setProperty(
+      "flex-basis",
+      `calc(var(--canvas-width) / ${size})`
+    );
   }
 });
